1aae6d960cdf05c9d764f9960fc8b741f4d072e9
galezra <galezra@gmail.com>
2018-01-07 20:22:33 +0200

    Main controller for adv list

---
+++ public/controllers/viewAdvCtrl.js
@@ -0,0 +1,43 @@
+
+"use strict";
+function viewAdvCtrl($scope,$rootScope,$location,advDetailsService ) {
+
+    if($location.path() == "/advDetails"){
+        advDetailsService.getAllAdvert().then(function (value) {
+            $scope.msgs = value.data;
+            $scope.msgEdit = {};
+
+        });
+    }
+    $scope.delete = function (_id) {
+        swal({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then(function (result) {
+            if(result.value)
+            {
+                swal(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                );
+                advDetailsService.deleteAdvert(_id).then(function () {
+                    $location.path('/manager');
+                    $scope.$apply();
+
+                });
+            }
+
+        });
+    };
+
+}
+angular.module('myApp').controller('viewAdvCtrl',[ '$scope','$rootScope','$location','advDetailsService',viewAdvCtrl]);
+
+
+
