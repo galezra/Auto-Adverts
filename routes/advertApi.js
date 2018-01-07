a7cbed80b0ee6d35c23435ec251fce6f40cf412b
galezra <galezra@gmail.com>
2018-01-07 20:18:44 +0200

    Add create/edit/upload requests

--- routes/advertApi.js
+++ routes/advertApi.js
@@ -1,4 +1,6 @@
-var db = require('../accessDB/advertDB')
+var db = require('../accessDB/advertDB');
+var formidable = require('formidable');
+var fs = require('fs');
 
 exports.getAllAdverts = function (req, res) {
     db.getAllAdverts(function(err, advert) {
@@ -8,6 +10,19 @@ exports.getAllAdverts = function (req, res) {
             });
         } else {
             console.log('*** GetAllAdverts API OK');
+            res.json(advert);
+        }
+    });
+};
+
+exports.createAdvert = function (req, res) {
+    var newAdvert = req.body;
+    db.createAdvert(newAdvert,function(err ,advert) {
+        if (err) {
+            console.log('*** CreateAdvert API Err');
+            res.json({'status': false});
+        } else {
+            console.log('*** CreateAdvert API OK');
             res.json(advert);
         }
     });
@@ -36,6 +51,34 @@ exports.editAdvert = function (req, res) {
             console.log('*** EditAdvert API OK');
             res.json(advert);
         }
+    });
+};
+
+exports.updateAdvert = function (req, res) {
+    console.log('*** UpdateAdvert API');
+    var editAdvert = req.body;
+    db.updateAdvert(editAdvert,function(err ,advert) {
+        if (err) {
+            console.log('*** UpdateAdvert API Err');
+            res.json({'status': false});
+        } else {
+            console.log('*** UpdateAdvert API OK');
+            res.json(advert);
+        }
+    });
+};
+
+exports.upload = function (req,res) {
+    console.log('*** UploadImage API OK');
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var oldpath = files.file.path;
+        var newpath = 'public/images/uploads/' + files.file.name;
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+        });
     });
 };
 
