f2f902e0d93f9904811ff99500a842f51115890d
galezra <galezra@gmail.com>
2018-02-05 20:43:09 +0200

    Add settings requests

--- accessDB/advertDB.js
+++ accessDB/advertDB.js
@@ -18,6 +18,21 @@ var local = module.exports = {
         });
     },
 
+    createAdvert: function (advert,callback) {
+        var newAdvert = advert;
+        console.log(newAdvert);
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var messagesDB = db.db('myDB');
+            messagesDB.collection("messages").insertOne(newAdvert,function (err,result) {
+                if (err) throw err;
+                callback(null, result);
+                db.close();
+
+            })
+        });
+    },
+
     deleteAdvertById: function (_id,callback) {
         var myquery = {"_id" : ObjectId(_id)};
         MongoClient.connect(url, function (err, db) {
@@ -44,7 +59,80 @@ var local = module.exports = {
 
             })
         });
-    }
+    },
 
+    updateAdvert: function (editAdvert,callback) {
+        var myquery = {"_id" : ObjectId(editAdvert._id)};
+        editAdvert._id = ObjectId(editAdvert._id);
+        var newEditAdvert = editAdvert;
+        console.log(newEditAdvert);
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var messagesDB = db.db('myDB');
+            messagesDB.collection("messages").replaceOne(myquery,newEditAdvert,function (err,result) {
+                if (err) throw err;
+                callback(null, result);
+                db.close();
 
+            })
+        });
+    },
+
+    getSettings: function (callback) {
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var messagesDB = db.db('myDB');
+            messagesDB.collection("settings").find({name:"init"}).toArray(function (err, result) {
+                if (err) throw err;
+                callback(null, result);
+                db.close();
+            });
+
+        });
+    },
+
+    getLocation: function (callback) {
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var messagesDB = db.db('myDB');
+            messagesDB.collection("settings").find({name:"googleMap"}).toArray(function (err, result) {
+                if (err) throw err;
+                callback(null, result);
+                db.close();
+            });
+
+        });
+    },
+
+    updateAdvSettings: function (updateSettings,callback) {
+        var myquery = {"_id" : ObjectId(updateSettings._id)};
+        updateSettings._id = ObjectId(updateSettings._id);
+        var newUpdateSettings = updateSettings;
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var messagesDB = db.db('myDB');
+            messagesDB.collection("settings").replaceOne(myquery,newUpdateSettings,function (err,result) {
+                if (err) throw err;
+                callback(null, result);
+                db.close();
+
+            })
+        });
+    },
+
+    getSearchScreen: function (searchId,callback) {
+        MongoClient.connect(url, function (err, db) {
+            var id = parseInt(searchId);
+            var query = {id: id};
+            if (err) throw err;
+            var messagesDB = db.db('myDB');
+            messagesDB.collection("messages").find(query).toArray(function (err, result) {
+                console.log(result);
+                if (err) throw err;
+                callback(null, result);
+                db.close();
+            });
+
+        });
+    },
 };
