a02aa3006d269503d7f4144d808cd1d692bc4a0d
galezra <galezra@gmail.com>
2018-02-05 20:32:15 +0200

    Fix

--- public/service/advDetailsService.js
+++ public/service/advDetailsService.js
@@ -1,63 +1,44 @@
 "use strict";
     function advDetailsService($http) {
+
         this.getAllAdvert = function () {
           return $http.get('/api/dataservice/GetAllAdverts');
          };
 
-         this.deleteAdvert = function (_id) {
-            return $http.delete('/api/dataservice/DeleteAdverts/'+_id)
-         };
+        this.createAdvert = function (newAdvert) {
+            return $http.post('/api/dataservice/CreateAdvert', newAdvert);
 
-         this.editAdvert = function (_id) {
+        };
+
+         this.editAdvert1 = function (_id) {
             return $http.get('/api/dataservice/EditAdvert/'+_id)
 
-         }
-     }
+         };
 
-     angular.module('myApp').service('advDetailsService',['$http',advDetailsService]);
+        this.updateAdvert = function (editAdvert) {
+            return $http.put('/api/dataservice/UpdateAdvert', editAdvert);
+
+        };
+
+        this.deleteAdvert = function (_id) {
+            return $http.delete('/api/dataservice/DeleteAdverts/'+_id)
+        };
+
+        this.getSettings = function () {
+            return $http.get('/api/dataservice/GetSettings');
+        };
+
+        this.updateAdvSettings = function (newSettings) {
+            return $http.post('/api/dataservice/updateAdvSettings/',newSettings);
+        };
+
+        this.getSearchScreen = function (id) {
+            return $http.get('/api/dataservice/GetSearchId/'+id);
+
+        }
+    }
 
 
-// angular.module('myApp').service('advDetailsService',function ($http) {
-//     this.getAllAdvert = function () {
-//         return $http.get('/api/dataservice/GetAllAdverts').then(function (value) {
-//             return value.data;
-//
-//         })
-//
-//     }
-//
-// });
 
-// (function () {
-//     "use strict";
-//     function advDetailsService($http) {
-//         this.getAllAdvert = function(){
-//             return $http.get('/api/dataservice/GetAllAdverts');
-//         };
-//         // this.getAdvertById = function(id){
-//         //     return $http.get('/api/dataservice/GetAdvertById/' + id);
-//         // };
-//         // this.addAdvert = function(advert){
-//         //     return $http.post('/api/dataservice/CreateAdvert/', advert);
-//         // };
-//         // this.saveAdvert = function(advert){
-//         //     return $http.put('/api/dataservice/EditAdvert/', advert);
-//         // };
-//         // this.deleteAdvert = function(id){
-//         //     return $http.delete('/api/dataservice/DeleteAdvertById/' + id);
-//         // };
-//     }
-//
-//     angular.module('myApp').service('advDetailsService', ['$http',advDetailsService]);
-// })();
-//     "use strict";
 
-// function advDetailsService($http) {
-//     return {
-//         getAllAdvert: function () {
-//             return $http.get('/api/dataservice/GetAllAdverts');
-//         }
-//     };
-// }
-//
-// angular.module('myApp').factory('advDetailsService',['$http',advDetailsService]);
+     angular.module('manageAdv').service('advDetailsService',['$http',advDetailsService]);
